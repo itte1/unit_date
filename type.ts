242586f846e290da9dedb5b5d8aaa28b980f383f
itte1 <nakamura@itte.dev>
2023-03-30 18:58:52 +0900

    Add a get function

--- type.ts
+++ type.ts
@@ -9,6 +9,47 @@ export type DateObject = {
 
 export type DateUnit = keyof(DateObject)
 
+export type YearDigitDate = {
+  years: number
+}
+
+export type MonthDigitDate = {
+  years: number
+  months: number
+}
+
+export type DayDigitDate = {
+  years: number
+  months: number
+  days: number
+}
+
+export type HourDigitDate = {
+  years: number
+  months: number
+  days: number
+  hours: number
+}
+
+export type MinuteDigitDate = {
+  years: number
+  months: number
+  days: number
+  hours: number
+  minutes: number
+}
+
+export type FullDigitDate = {
+  years: number
+  months: number
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+}
+
+export type AnyDigitDate = YearDigitDate | MonthDigitDate | DayDigitDate | HourDigitDate | MinuteDigitDate | FullDigitDate
+
 export type DateOfYear = {
   months: number // 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
   days?: number // 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30 | 31
