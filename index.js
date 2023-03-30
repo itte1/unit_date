477077bd4f7fd065f748c853d21043e92617e1d3
itte1 <nakamura@itte.dev>
2023-03-30 22:21:11 +0900

    Build

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ var mod_exports = {};
 __export(mod_exports, {
   add: () => add,
   diff: () => diff,
+  format: () => format,
+  get: () => get,
   next: () => next,
   set: () => set,
   unit: () => unit
@@ -45,6 +47,25 @@ function unit(date, unit2) {
     default:
       return new Date(date);
   }
+}
+
+// get.ts
+function get(date, unit2) {
+  let newDate = { years: date.getFullYear() };
+  switch (unit2) {
+    default:
+      newDate.seconds = date.getSeconds();
+    case "minutes":
+      newDate.minutes = date.getMinutes();
+    case "hours":
+      newDate.hours = date.getHours();
+    case "days":
+      newDate.days = date.getDate();
+    case "months":
+      newDate.months = date.getMonth() + 1;
+    case "years":
+  }
+  return newDate;
 }
 
 // set.ts
@@ -135,4 +156,74 @@ function next(date, value, unitForDiff) {
   let unitDate = unit(date, unit2);
   let dateOfThis = set(unitDate, value);
   return diff(date, dateOfThis, unitForDiff) < 0 ? dateOfThis : set(add(unitDate, { [unit2]: 1 }), value);
+}
+
+// format.ts
+function toLocaleDateFormat(date, locales, options) {
+  return new Intl.DateTimeFormat(locales, options).format(date);
+}
+function format(date, format2 = "YYYY-MM-DDTHH:mm:sszzz", locales) {
+  return format2.replace(/y{1,5}|Y{4}|M{1,4}|d{3,4}|D{1,2}|H{1,2}|h{1,4}|m{1,2}|s{1,2}|f{1,3}|z{1,3}/g, (m) => {
+    switch (m) {
+      case "yy":
+      case "yyyy":
+        return ("0000" + date.getFullYear()).slice(-m.length);
+      case "YYYY":
+        return date.getFullYear() + "";
+      case "M":
+        return date.getMonth() + 1 + "";
+      case "MM":
+        return ("0" + (date.getMonth() + 1)).slice(-2);
+      case "MMM":
+        return toLocaleDateFormat(date, locales, { month: "short" });
+      case "MMMM":
+        return toLocaleDateFormat(date, locales, { month: "long" });
+      case "D":
+        return date.getDate() + "";
+      case "DD":
+        return ("0" + date.getDate()).slice(-2);
+      case "ddd":
+        return toLocaleDateFormat(date, locales, { weekday: "short" });
+      case "dddd":
+        return toLocaleDateFormat(date, locales, { weekday: "long" });
+      case "H":
+        return date.getHours() + "";
+      case "HH":
+        return ("0" + date.getHours()).slice(-2);
+      case "h":
+        return date.getHours() % 12 + "";
+      case "hh":
+        return ("0" + date.getHours() % 12).slice(-2);
+      case "hhh":
+        return toLocaleDateFormat(date, locales, { hour: "numeric", hour12: true });
+      case "hhhh":
+        return toLocaleDateFormat(date, locales, { hour: "2-digit", hour12: true });
+      case "m":
+        return date.getMinutes() + "";
+      case "mm":
+        return ("0" + date.getMinutes()).slice(-2);
+      case "s":
+        return date.getSeconds() + "";
+      case "ss":
+        return ("0" + date.getSeconds()).slice(-2);
+      case "f":
+        return Math.floor(date.getMilliseconds() / 100) + "";
+      case "ff":
+        return ("0" + Math.floor(date.getMilliseconds() / 10)).slice(-2);
+      case "fff":
+        return ("00" + date.getMilliseconds()).slice(-3);
+      case "z":
+      case "zz":
+      case "zzz": {
+        let offset = date.getTimezoneOffset();
+        let sign = offset >= 0 ? "-" : "+";
+        let value = Math.abs(date.getTimezoneOffset());
+        let hours = ("0" + Math.floor(value / 60)).slice(-2);
+        let minutes = ("0" + value % 60).slice(-2);
+        return m === "z" ? sign + hours : m === "zz" ? sign + hours + minutes : sign + hours + ":" + minutes;
+      }
+      default:
+        return "";
+    }
+  });
 }
