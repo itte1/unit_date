a2d2502844c6c3982edf8f0c6782e4a2b8a3d9dd
itte1 <nakamura@itte.dev>
2023-05-11 18:34:39 +0900

    Build npm files

--- _build.ts
+++ _build.ts
@@ -18,4 +18,21 @@ await esbuild.build({
   format: 'cjs',
 })
 
+await esbuild.build({
+  plugins: [denoPlugin()],
+  entryPoints: ['./utc/mod.ts'],
+  outfile: './utc/unit_date.js',
+  bundle: true,
+  minify: true,
+  format: 'esm',
+})
+
+await esbuild.build({
+  plugins: [denoPlugin()],
+  entryPoints: ['./utc/mod.ts'],
+  outfile: './utc/index.js',
+  bundle: true,
+  format: 'cjs',
+})
+
 esbuild.stop()
